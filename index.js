3566f651aa0d8a7d4ffe58abd15bcc6ebe466fad
dudushy <eduardotalarico1337@gmail.com>
2025-08-31 18:12:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth} = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
 
 client.on('qr', (qr) => {
   qrcode.generate(qr, { small: true });
@@ -12,7 +14,7 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.body == '!ping') {
+  if (msg.body == '/ping') {
     msg.reply('pong');
   }
 });
