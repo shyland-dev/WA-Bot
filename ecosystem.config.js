4421eb14f9b789c24949265c8265d7cb05e8824e
dudushy <eduardotalarico1337@gmail.com>
2025-10-04 19:03:42 -0300

    Update ecosystem and package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,25 +7,8 @@ module.exports = {
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
-        env: {
-            NODE_ENV: 'development'
-        },
         env_production: {
             NODE_ENV: 'production'
-        }
-    }, {
-        name: 'wa-bot-dev',
-        script: 'src/index.ts',
-        interpreter: 'node',
-        interpreter_args: '--loader ts-node/esm',
-        cwd: './',
-        instances: 1,
-        autorestart: true,
-        watch: ['src'],
-        ignore_watch: ['node_modules', 'data', '.wwebjs_auth', '.wwebjs_cache'],
-        max_memory_restart: '1G',
-        env: {
-            NODE_ENV: 'development'
         }
     }]
 };
